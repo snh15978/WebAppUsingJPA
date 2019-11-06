d5f738c3e66c00dd1e3ff5fea11895a2ec5cb4e6
namhyun Sung <snh15978@gmail.com>
2019-11-07 01:25:42 +0900

    회원, 상품, 주문 도메인 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,6 +1,9 @@
 package jpabook.jpashop.domain;
 
+import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,6 +12,7 @@ import static javax.persistence.FetchType.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id
@@ -27,4 +31,30 @@ public class OrderItem {
     private int orderPrice;
 
     private int count;
+
+    //생성 메소드
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+    //비즈니스 로직
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+
+
+    //조회 로직
+
+    /*
+     *  주문상품 금액 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice()*getCount();
+    }
 }
